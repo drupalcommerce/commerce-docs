f3ff977f6b350be0384a4dbbd84527724180e216
Matt Glaman <nmd.matt@gmail.com>
2017-01-27 14:26:02 -0600

    Use ReadTheDocs theme for now

--- _build/conf.py
+++ _build/conf.py
@@ -91,18 +91,17 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-# import sphinx_rtd_theme
+import sphinx_rtd_theme
 
-# html_theme = "sphinx_rtd_theme"
-html_theme = "classic"
+html_theme = "sphinx_rtd_theme"
 
-# html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
