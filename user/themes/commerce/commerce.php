c62462ae1ba9bcd49bd10719c06b25acafc3b225
Matt Glaman <nmd.matt@gmail.com>
2018-03-20 23:02:39 -0500

    Add SEO values to pages

--- user/themes/commerce/commerce.php
+++ user/themes/commerce/commerce.php
@@ -1,9 +1,60 @@
 <?php
 namespace Grav\Theme;
 
+use Grav\Common\Page\Page;
 use Grav\Common\Theme;
 
 class Commerce extends Theme
 {
 
+  public static function getSubscribedEvents()
+  {
+    return [
+      'onThemeInitialized' => ['onThemeInitialized', 0]
+    ];
+  }
+
+  public function onThemeInitialized()
+  {
+    if ($this->isAdmin()) {
+      $this->active = false;
+      return;
+    }
+
+    $this->enable([
+      'onPageInitialized' => ['onPageInitialized', 0]
+    ]);
+  }
+
+  /**
+   * Add content after page content was read into the system.
+   *
+   */
+  public function onPageInitialized()
+  {
+    /** @var \Grav\Common\Page\Page $page */
+    $page = $this->grav['page'];
+    $config = $this->mergeConfig($page);
+    $metadata = $page->metadata();
+    $summary = trim(strip_tags($page->summary(null, true)));
+
+    $metadata_values = [
+      'og:sitename' => $config->get('site.title'),
+      'og:title' => $page->title(),
+      'og:description' => $summary,
+      'twitter:card' => 'summary',
+      'twitter:title' => $page->title(),
+      'twitter:description' => $summary,
+      'twitter:site' => '@drupalcommerce',
+      'twitter:label1' => 'Filed under',
+      'twitter:data1' => $page->parent()->title(),
+    ];
+    foreach ($metadata_values as $property => $value) {
+      $metadata[$property]['name'] = $property;
+      $metadata[$property]['property'] = $property;
+      $metadata[$property]['content'] = $value;
+    }
+
+    $page->metadata($metadata);
+  }
 }
